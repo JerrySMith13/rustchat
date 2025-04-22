13bcc8b51a60dfd378218c3f3b56bcd28537421a
Jeremy Smith <jeremy.ws.2008@gmail.com>
2025-04-22 10:54:50 -0700

    Delete rustchat-name-server/target directory

--- rustchat-name-server/target/debug/deps/rustchat_name_server-f321a8efbd797d68.d
+++
@@ -1,5 +0,0 @@
-/home/jerem/rustchat/rustchat-name-server/target/debug/deps/librustchat_name_server-f321a8efbd797d68.rmeta: src/main.rs
-
-/home/jerem/rustchat/rustchat-name-server/target/debug/deps/rustchat_name_server-f321a8efbd797d68.d: src/main.rs
-
-src/main.rs:
