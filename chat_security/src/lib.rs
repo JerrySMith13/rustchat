6bafc40935d20878511505ba661b3439f2197533
= <jeremy.ws.2008@gmail.com>
2025-04-25 13:57:48 -0500

    worked more on send_message function

--- chat_security/src/lib.rs
+++ chat_security/src/lib.rs
@@ -1,16 +1,16 @@
 use std::net::TcpStream;
 use std::io::{Read, Write};
 
+
 use bincode::serialize;
-use rand::{rngs::OsRng, CryptoRng};
-use rand::RngCore;
+use rand::rngs::OsRng;
 use serde::{Deserialize, Serialize};
-use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
+use x25519_dalek::{EphemeralSecret, PublicKey};
 use sha2::Sha256;
 use hkdf::Hkdf;
 use chacha20poly1305::{
-    aead::{Aead, KeyInit, OsRng as AeadOsRng},
-    XChaCha20Poly1305, XNonce,
+    aead::{Aead, KeyInit, AeadCore},
+    XChaCha20Poly1305
 };
 
 
@@ -53,9 +53,11 @@ impl HandshakeData{
         return Ok(response);
     }
 
-    fn recieve_handshake(stream: &mut TcpStream) -> Result<HandshakeData, std::io::Error>{
+    fn recieve_handshake(stream: &mut TcpStream, pub_key: PublicKey) -> Result<HandshakeData, std::io::Error>{
         let buf = Self::read_length_prefixed(stream)?;
         let response: HandshakeData = bincode::deserialize(&buf)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+        let buf = bincode::serialize(&HandshakeData{ public_key: pub_key.to_bytes() })
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
         Self::write_length_prefixed(stream, &buf)?;
         return Ok(response);
@@ -70,10 +72,29 @@ struct EncryptedMessage {
     ciphertext: Vec<u8>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct Message{
+    sender_id: String,
+    to_id: String,
+    contents: String,
+    timestamp: u64,
+}
+impl Message{
+    
+    pub fn from_string(message: String) -> Self{
+        todo!("Function not yet implemented, but will parse text data into a message object for easier reading");
+    }
+
+    pub fn to_string(&self) -> String{
+        todo!("Function not yet implemented, but will parse a message object into a string for easier sending");
+    }
+
+}
+
 
 struct SessionCryptData{
-    shared_key: [u8; 32],
     cipher: XChaCha20Poly1305,
+
 }
 
 #[allow(dead_code)]
@@ -101,18 +122,19 @@ impl SessionCryptData{
 
 
         return Ok(SessionCryptData{
-            shared_key: shared,
             cipher,
         });
     
     }
 
     fn recieve_session(stream: &mut TcpStream) -> Result<Self, std::io::Error>{
-        let peer_public = HandshakeData::recieve_handshake(stream)?;
-        let peer_public = PublicKey::from(peer_public.public_key);
-
         let self_secret = EphemeralSecret::random_from_rng(OsRng);
         let self_public = PublicKey::from(&self_secret);
+
+        let peer_public = HandshakeData::recieve_handshake(stream, self_public)?;
+        let peer_public = PublicKey::from(peer_public.public_key);
+
+        
 
         let shared = self_secret.diffie_hellman(&peer_public);
         let shared = Self::derive_key(shared.as_bytes());
@@ -120,12 +142,26 @@ impl SessionCryptData{
         let cipher = XChaCha20Poly1305::new_from_slice(&shared).unwrap();
 
         return Ok(SessionCryptData{
-            shared_key: shared,
             cipher,
         });
     }
 
+    fn send_message(&self, stream: &mut TcpStream, message: Message) -> Result<(), std::io::Error>{
 
+        let nonce = XChaCha20Poly1305::generate_nonce(&mut OsRng);
+        let msg_bytes = serialize(&message).expect("Failed to serialize message");
+        let ciphertext = self.cipher.encrypt(&nonce, msg_bytes.as_ref())
+            .expect("Failed to encrypt message");
+        let encrypted_message = EncryptedMessage {
+            nonce: *nonce.as_ref(),
+            ciphertext,
+        };
+        let serialized = serialize(&encrypted_message).expect("Failed to serialize encrypted message");
+        HandshakeData::write_length_prefixed(stream, &serialized)?;
+        Ok(())
+
+
+    }
 
     
 }
